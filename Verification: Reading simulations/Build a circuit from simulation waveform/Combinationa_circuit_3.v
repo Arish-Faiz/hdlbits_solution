c51d37b00ef6c538f5f240635fc59459f76ef0fe
Arish-Faiz <faz.arishafsaf@gmail.com>
2025-09-02 09:57:55 +0530

    Create Combinationa_circuit_3.v

---
+++ Verification: Reading simulations/Build a circuit from simulation waveform/Combinationa_circuit_3.v
@@ -0,0 +1,14 @@
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output q );//
+
+  //assign q = (b&d) || (b&c) || (a&c)  || (a&d);  this is how you do it by looking at the waveform directly
+  // q = (b & (c | d)) | (a & (c | d))
+  // q = (a | b) & (c | d)
+  assign q = (a | b) & (c | d); // this is after simplification
+	
+
+endmodule
