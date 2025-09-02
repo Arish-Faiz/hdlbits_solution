761d09399f5a2de84c2ad8560a15029765b1f181
Arish-Faiz <faz.arishafsaf@gmail.com>
2025-09-02 09:41:12 +0530

    Update Combinational_Circuit_2.v

--- Verification: Reading simulations/Build a circuit from simulation waveform/Combinational_Circuit_2.v
+++ Verification: Reading simulations/Build a circuit from simulation waveform/Combinational_Circuit_2.v
@@ -8,3 +8,6 @@ module top_module (
     assign q = ~(a^b^c^d);
 
 endmodule
+// That’s exactly when the number of 1s among {a,b,c,d} is even.
+//So the line is just an even-parity detector:
+// assign q = ( (a+b+c+d == 0) || (a+b+c+d == 2) || (a+b+c+d == 4) ); other way to write the same thing.
