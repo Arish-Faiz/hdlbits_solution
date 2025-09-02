0c9b5d7862881ef07c4ea32f38fb793be6a3d941
Arish-Faiz <faz.arishafsaf@gmail.com>
2025-09-02 10:13:07 +0530

    Update Combinationa_circuit_3.v

--- Verification: Reading simulations/Build a circuit from simulation waveform/Combinationa_circuit_3.v
+++ Verification: Reading simulations/Build a circuit from simulation waveform/Combinationa_circuit_3.v
@@ -8,7 +8,7 @@ module top_module (
   //assign q = (b&d) || (b&c) || (a&c)  || (a&d);  this is how you do it by looking at the waveform directly
   // q = (b & (c | d)) | (a & (c | d))
   // q = (a | b) & (c | d)
-  assign q = (a | b) & (c | d); // this is after simplification
+	assign q = (a | b) & (c | d); // this is after simplification. This produces exactly the same logic, but is easier to read and the synthesizer will recognize it immediately.
 	
 
 endmodule
